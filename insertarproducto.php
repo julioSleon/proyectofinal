e5f437178574adfcf45ac552d49ff8a410aba5bd
BDestuardo <amestuardo@hotmail.com>
2015-10-14 20:01:54 -0600

    subo la estructura del proyecto

---
+++ insertarproducto.php
@@ -0,0 +1,158 @@
+<!DOCTYPE HTML>
+<head>
+<title>Free Ecomm Template Website Template | Home :: w3layouts</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link href='http://fonts.googleapis.com/css?family=Ubuntu+Condensed' rel='stylesheet' type='text/css'>
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
+<script type="text/javascript" src="js/jquery-1.9.0.min.js"></script> 
+<script src="js/jquery.openCarousel.js" type="text/javascript"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+<script type="text/javascript" src="js/move-top.js"></script>
+</head>
+<body>
+	<div class="header">
+	
+		<div class="indication">
+			<br><b>SIGUENOS</b></br>
+			<a href="https://www.facebook.com"><img src="images/facebook.ico" width="30" height="30"></a>
+		<a href="https://twitter.com"><img src="images/twitter.ico" width="30" height="30"></a>
+		</div>
+  	  		<div class="wrap">
+				<div class="header_top">
+					<div class="logo">
+						<a href="index.html"><img src="images/logo.png" w /></a>
+					</div>
+						<div class="header_top_right">
+							  <div class="search_box">
+							  	<span>Buscar</span>
+					     		<form>
+					     			<input type="text" value=""><input type="submit" value="">
+					     		</form>
+					     		<div class="clear"></div>
+					     	</div>
+					</div>
+			     <div class="clear"></div>
+  		    </div>     
+  		    <div class="navigation">
+  		    	<a class="toggleMenu" href="#">Menu</a>
+					<ul class="nav">
+						<li>
+							<a href="index.html">INICIO</a>
+						</li>
+						<li  class="test">
+							<a href="#">INFORMÁTICA</a>
+							<ul>
+								<li>
+									<a href="#">PC</a>
+									
+								</li>
+								<li>
+									<a href="#">Portatiles</a>
+									
+								</li>
+								<li>
+									<a href="#">Tablets</a>
+								</li>
+								<li>
+									<a href="#"> Accessorios</a>
+								</li>
+							</ul>
+						</li>
+						<li>
+							<a href="#">SMARTPHONES</a>
+							<ul>
+								<li>
+									<a href="#">Smartphones</a>
+									
+								</li>
+								<li>
+									<a href="#">Telefonia fija</a>
+
+								</li>
+							</ul>
+						</li>
+						<li>
+							<a href="#">VIDEOJUEGOS</a>
+							<ul>
+								<li><a href="#">PlayStation</a></li>
+								<li><a href="#">Xbox 360</a></li>
+								<li><a href="#">Nintendo Wii</a></li>
+								<li><a href="#">PSP</a></li>
+								
+							</ul>
+						</li>
+						<li>
+							<a href="#">IMAGEN/SONIDO</a>
+							<ul>
+								<li>
+									<a href="#">Imagen</a>
+								</li>
+								<li>
+									<a href="#">Sonido</a>
+								</li>
+								<li>
+									<a href="#">Reproductores</a>
+								</li>
+                                
+                                <li>
+									<a href="#">Audio Portatil</a>
+								</li>
+                                
+                                <li>
+									<a href="#">Accesorios</a>
+								</li>
+
+
+							</ul>
+						</li>
+						<li>
+							<a href="#">NOSOTROS</a>
+							<ul>
+								<li>
+									<a href="vision.html">Vision</a>
+
+								</li>
+								<li>
+									<a href="mision.html">Mision</a>
+									
+								</li>
+								
+							</ul>
+						</li>
+					</ul>
+					 <span class="left-ribbon"> </span> 
+      				 <span class="right-ribbon"> </span>    
+  		    </div>
+  		     <div class="header_bottom">
+			   <div class="slider-text">
+			   	<h2>Administrar Productos X-tech </h2>
+			   	<form name="form1" method="post" action="">
+			   	  <label >Nombre Del Producto</label>
+			   	  <input type="text" name="NomProducto" id="NomProducto">
+		   	     </form>
+			   	<p>&nbsp;</p>
+			   	<p>&nbsp;</p>
+                
+			   	
+			   	<a href="#">Somos tu mejor opcion ...</a>
+	  	      </div>
+	  	      <div class="slider-img">
+	  	      	<img src="images/vision.png" alt="" />
+	  	      </div>
+	  	     <div class="clear"></div>
+	      </div>
+   		</div>
+   </div>
+   
+    <script type="text/javascript">
+		$(document).ready(function() {			
+			$().UItoTop({ easingType: 'easeOutQuart' });
+			
+		});
+	</script>
+    <a href="#" id="toTop"> </a>
+    <script type="text/javascript" src="js/navigation.js"></script>
+</body>
+</html>
+
